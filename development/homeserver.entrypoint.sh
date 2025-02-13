028f1f44c2cb3c57d93c7bbc13054c18d6ad8f27
James Browning <jamesbrowning91@gmail.com>
2025-02-13 17:23:41 +0000

    remove entrypoint for homeserver as testnet is now a separate binary

--- development/homeserver.entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Check the NETWORK environment variable and execute the appropriate command
-if [ "$NETWORK" = "mainnet" ]; then
-  exec homeserver --config=/config.toml -t pubky_homeserver=info,tower_http=info
-else
-  exec testnet --config=/config.toml -t pubky_homeserver=info,tower_http=info
-fi
